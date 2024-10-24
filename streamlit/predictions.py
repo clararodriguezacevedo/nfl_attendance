4dc6a7dfceca9c2bdb55ae0a5ea3aaabf1e62232
Clara Rodriguez Acevedo <78107786+clararodriguezacevedo@users.noreply.github.com>
2024-10-24 12:01:40 -0300

    Testing

--- streamlit/predictions.py
+++ streamlit/predictions.py
@@ -149,231 +149,229 @@ regular_scores["precision"]["ridge"] = ridge_score
 regular_scores["time"]["ridge"] = ridge_time
 
 def process_input_data():
-    try:
-        # Serie de diccionarios para mappear los variables no dadas a partir de las dadas (por ejemplo, el estadio y la ciudad a partir del equipo local)
-        days_week = {
-            0: "Mon",
-            1: "Tue",
-            2: "Wed",
-            3: "Thu",
-            4: "Fri",
-            5: "Sat",
-            6: "Sun",
-        }
 
-        home_team_names = {
-            "Chiefs": "Kansas City Chiefs",
-            "Raiders": "Oakland Raiders",
-            "Falcons": "Atlanta Falcons",
-            "Bills": "Buffalo Bills",
-            "Steelers": "Pittsburgh Steelers",
-            "Ravens": "Baltimore Ravens",
-            "Eagles": "Philadelphia Eagles",
-            "Lions": "Detroit Lions",
-            "Jaguars": "Jacksonville Jaguars",
-            "Rams": "Los Angeles Rams",
-            "Panthers": "Carolina Panthers",
-            "Packers": "Green Bay Packers",
-            "Cowboys": "Dallas Cowboys",
-            "Vikings": "Minnesota Vikings",
-            "Broncos": "Denver Broncos",
-            "Texans": "Houston Texans",
-            "Cardinals": "Arizona Cardinals",
-            "Patriots": "New England Patriots",
-            "Buccaneers": "Tampa Bay Buccaneers",
-            "Titans": "Tennessee Titans",
-            "Dolphins": "Miami Dolphins",
-            "Seahawks": "Seattle Seahawks",
-            "Redskins": "Washington Redskins",
-            "Commanders": "Washington Redskins",
-            "Jets": "New York Jets",
-            "Colts": "Indianapolis Colts",
-            "Bears": "Chicago Bears",
-            "Saints": "New Orleans Saints",
-            "49ers": "San Francisco 49ers",
-            "Browns": "Cleveland Browns",
-            "Bengals": "Cincinnati Bengals",
-            "Chargers": "Los Angeles Chargers",
-            "Giants": "New York Giants",
-        }
+    # Serie de diccionarios para mappear los variables no dadas a partir de las dadas (por ejemplo, el estadio y la ciudad a partir del equipo local)
+    days_week = {
+        0: "Mon",
+        1: "Tue",
+        2: "Wed",
+        3: "Thu",
+        4: "Fri",
+        5: "Sat",
+        6: "Sun",
+    }
 
-        stadium_names = {
-            "Bears": "Soldier Field",
-            "Packers": "Lambeau Field",
-            "Chiefs": "Arrowhead",
-            "Bills": "Highmark",
-            "Saints": "Caesars Superdome",
-            "Dolphins": "Hard Rock",
-            "Jaguars": "EverBank",
-            "Panthers": "Bank of America",
-            "Redskins": "Northwest",
-            "Ravens": "M&T Bank",
-            "Buccaneers": "Raymond James",
-            "Browns": "Huntington Bank",
-            "Titans": "Nissan",
-            "Bengals": "Paycor",
-            "Broncos": "Empower Field at Mile High",
-            "Steelers": "Acrisure",
-            "Lions": "Ford Field",
-            "Texans": "NRG",
-            "Patriots": "Gillette",
-            "Seahawks": "Lumen Field",
-            "Eagles": "Lincoln Financial Field",
-            "Cardinals": "State Farm",
-            "Colts": "Lucas Oil",
-            "Cowboys": "AT&T",
-            "Jets": "MetLife",
-            "Giants": "MetLife",
-            "49ers": "Levi's",
-            "Vikings": "US Bank",
-            "Falcons": "Mercedes-Benz",
-            "Raiders": "Oakland Coliseum",
-            "Rams": "Los Angeles Memorial Coliseum",
-            "Chargers": "Dignity Health Sports Park",
-        }
+    home_team_names = {
+        "Chiefs": "Kansas City Chiefs",
+        "Raiders": "Oakland Raiders",
+        "Falcons": "Atlanta Falcons",
+        "Bills": "Buffalo Bills",
+        "Steelers": "Pittsburgh Steelers",
+        "Ravens": "Baltimore Ravens",
+        "Eagles": "Philadelphia Eagles",
+        "Lions": "Detroit Lions",
+        "Jaguars": "Jacksonville Jaguars",
+        "Rams": "Los Angeles Rams",
+        "Panthers": "Carolina Panthers",
+        "Packers": "Green Bay Packers",
+        "Cowboys": "Dallas Cowboys",
+        "Vikings": "Minnesota Vikings",
+        "Broncos": "Denver Broncos",
+        "Texans": "Houston Texans",
+        "Cardinals": "Arizona Cardinals",
+        "Patriots": "New England Patriots",
+        "Buccaneers": "Tampa Bay Buccaneers",
+        "Titans": "Tennessee Titans",
+        "Dolphins": "Miami Dolphins",
+        "Seahawks": "Seattle Seahawks",
+        "Redskins": "Washington Redskins",
+        "Commanders": "Washington Redskins",
+        "Jets": "New York Jets",
+        "Colts": "Indianapolis Colts",
+        "Bears": "Chicago Bears",
+        "Saints": "New Orleans Saints",
+        "49ers": "San Francisco 49ers",
+        "Browns": "Cleveland Browns",
+        "Bengals": "Cincinnati Bengals",
+        "Chargers": "Los Angeles Chargers",
+        "Giants": "New York Giants",
+    }
 
-        stadium_max_capacities = {
-            "Bears": 66944,
-            "Packers": 81441,
-            "Chiefs": 76416,
-            "Bills": 80290,
-            "Saints": 76468,
-            "Dolphins": 70000,
-            "Jaguars": 82000,
-            "Panthers": 74867,
-            "Redskins": 67617,
-            "Ravens": 70745,
-            "Buccaneers": 74512,
-            "Browns": 54147,
-            "Titans": 69143,
-            "Bengals": 67260,
-            "Broncos": 76125,
-            "Steelers": 68400,
-            "Lions": 70000,
-            "Texans": 75000,
-            "Patriots": 71000,
-            "Seahawks": 72000,
-            "Eagles": 75000,
-            "Cardinals": 72200,
-            "Colts": 70000,
-            "Cowboys": 85000,
-            "Jets": 83367,
-            "Giants": 83367,
-            "49ers": 75000,
-            "Vikings": 73000,
-            "Falcons": 79330,
-            "Raiders": 63132,
-            "Rams": 93607,
-            "Chargers": 27000,
-        }
+    stadium_names = {
+        "Bears": "Soldier Field",
+        "Packers": "Lambeau Field",
+        "Chiefs": "Arrowhead",
+        "Bills": "Highmark",
+        "Saints": "Caesars Superdome",
+        "Dolphins": "Hard Rock",
+        "Jaguars": "EverBank",
+        "Panthers": "Bank of America",
+        "Redskins": "Northwest",
+        "Ravens": "M&T Bank",
+        "Buccaneers": "Raymond James",
+        "Browns": "Huntington Bank",
+        "Titans": "Nissan",
+        "Bengals": "Paycor",
+        "Broncos": "Empower Field at Mile High",
+        "Steelers": "Acrisure",
+        "Lions": "Ford Field",
+        "Texans": "NRG",
+        "Patriots": "Gillette",
+        "Seahawks": "Lumen Field",
+        "Eagles": "Lincoln Financial Field",
+        "Cardinals": "State Farm",
+        "Colts": "Lucas Oil",
+        "Cowboys": "AT&T",
+        "Jets": "MetLife",
+        "Giants": "MetLife",
+        "49ers": "Levi's",
+        "Vikings": "US Bank",
+        "Falcons": "Mercedes-Benz",
+        "Raiders": "Oakland Coliseum",
+        "Rams": "Los Angeles Memorial Coliseum",
+        "Chargers": "Dignity Health Sports Park",
+    }
 
-        stadium_regular_capacities = {
-            "Bears": 62500,
-            "Packers": 79704,
-            "Chiefs": 76416,
-            "Bills": 74000,
-            "Saints": 73208,
-            "Dolphins": 64767,
-            "Jaguars": 67814,
-            "Panthers": 74867,
-            "Redskins": 67617,
-            "Ravens": 70745,
-            "Buccaneers": 69218,
-            "Browns": 50805,
-            "Titans": 69143,
-            "Bengals": 65515,
-            "Broncos": 76125,
-            "Steelers": 68400,
-            "Lions": 65000,
-            "Texans": 72220,
-            "Patriots": 65878,
-            "Seahawks": 68740,
-            "Eagles": 69879,
-            "Cardinals": 63400,
-            "Colts": 63000,
-            "Cowboys": 80000,
-            "Jets": 82500,
-            "Giants": 82500,
-            "49ers": 68500,
-            "Vikings": 66655,
-            "Falcons": 71000,
-            "Raiders": 53200,
-            "Rams": 77500,
-            "Chargers": 27000,
-        }
+    stadium_max_capacities = {
+        "Bears": 66944,
+        "Packers": 81441,
+        "Chiefs": 76416,
+        "Bills": 80290,
+        "Saints": 76468,
+        "Dolphins": 70000,
+        "Jaguars": 82000,
+        "Panthers": 74867,
+        "Redskins": 67617,
+        "Ravens": 70745,
+        "Buccaneers": 74512,
+        "Browns": 54147,
+        "Titans": 69143,
+        "Bengals": 67260,
+        "Broncos": 76125,
+        "Steelers": 68400,
+        "Lions": 70000,
+        "Texans": 75000,
+        "Patriots": 71000,
+        "Seahawks": 72000,
+        "Eagles": 75000,
+        "Cardinals": 72200,
+        "Colts": 70000,
+        "Cowboys": 85000,
+        "Jets": 83367,
+        "Giants": 83367,
+        "49ers": 75000,
+        "Vikings": 73000,
+        "Falcons": 79330,
+        "Raiders": 63132,
+        "Rams": 93607,
+        "Chargers": 27000,
+    }
 
-        cities = {
-            "Patriots": "Foxborough,MA",
-            "Packers": "Green_Bay,WI",
-            "Chiefs": "Kansas_City,MO",
-            "Cowboys": "Arlington,TX",
-            "Rams": "Inglewood,CA",
-            "Buccaneers": "Tampa,FL",
-            "49ers": "Santa_Clara,CA",
-            "Bears": "Chicago,IL",
-            "Dolphins": "Miami_Gardens,FL",
-            "Jets": "East_Rutherford,NJ",
-            "Giants": "East_Rutherford,NJ",
-            "Raiders": "Paradise,NV",
-            "Broncos": "Denver,CO",
-            "Seahawks": "Seattle,WA",
-            "Cardinals": "Glendale,AZ",
-            "Eagles": "Philadelphia,PA",
-            "Bengals": "Cincinnati,OH",
-            "Browns": "Cleveland,OH",
-            "Steelers": "Pittsburgh,PA",
-            "Ravens": "Baltimore,MD",
-            "Falcons": "Atlanta,GA",
-            "Saints": "New_Orleans,LA",
-            "Panthers": "Charlotte,NC",
-            "Vikings": "Minneapolis,MN",
-            "Lions": "Detroit,MI",
-            "Colts": "Indianapolis,IN",
-            "Texans": "Houston,TX",
-            "Titans": "Nashville,TN",
-            "Jaguars": "Jacksonville,FL",
-            "Bills": "Orchard_Park,NY",
-            "Commanders": "Landover,MD",
-            "Redskins": "Landover,MD",
-            "Chargers": "Inglewood,CA",
-        }
+    stadium_regular_capacities = {
+        "Bears": 62500,
+        "Packers": 79704,
+        "Chiefs": 76416,
+        "Bills": 74000,
+        "Saints": 73208,
+        "Dolphins": 64767,
+        "Jaguars": 67814,
+        "Panthers": 74867,
+        "Redskins": 67617,
+        "Ravens": 70745,
+        "Buccaneers": 69218,
+        "Browns": 50805,
+        "Titans": 69143,
+        "Bengals": 65515,
+        "Broncos": 76125,
+        "Steelers": 68400,
+        "Lions": 65000,
+        "Texans": 72220,
+        "Patriots": 65878,
+        "Seahawks": 68740,
+        "Eagles": 69879,
+        "Cardinals": 63400,
+        "Colts": 63000,
+        "Cowboys": 80000,
+        "Jets": 82500,
+        "Giants": 82500,
+        "49ers": 68500,
+        "Vikings": 66655,
+        "Falcons": 71000,
+        "Raiders": 53200,
+        "Rams": 77500,
+        "Chargers": 27000,
+    }
 
-        sample_year = sample_date.year
-        sample_day = days_week[sample_date.weekday()]
-        sample_winner = home_team_names[sample_home_team]
-        sample_stadium_name = stadium_names[sample_home_team]
-        sample_stadium_max_capacity = stadium_max_capacities[sample_home_team]
-        sample_stadium_regular_capacity = stadium_regular_capacities[sample_home_team]
-        sample_home_city = cities[sample_home_team]
+    cities = {
+        "Patriots": "Foxborough,MA",
+        "Packers": "Green_Bay,WI",
+        "Chiefs": "Kansas_City,MO",
+        "Cowboys": "Arlington,TX",
+        "Rams": "Inglewood,CA",
+        "Buccaneers": "Tampa,FL",
+        "49ers": "Santa_Clara,CA",
+        "Bears": "Chicago,IL",
+        "Dolphins": "Miami_Gardens,FL",
+        "Jets": "East_Rutherford,NJ",
+        "Giants": "East_Rutherford,NJ",
+        "Raiders": "Paradise,NV",
+        "Broncos": "Denver,CO",
+        "Seahawks": "Seattle,WA",
+        "Cardinals": "Glendale,AZ",
+        "Eagles": "Philadelphia,PA",
+        "Bengals": "Cincinnati,OH",
+        "Browns": "Cleveland,OH",
+        "Steelers": "Pittsburgh,PA",
+        "Ravens": "Baltimore,MD",
+        "Falcons": "Atlanta,GA",
+        "Saints": "New_Orleans,LA",
+        "Panthers": "Charlotte,NC",
+        "Vikings": "Minneapolis,MN",
+        "Lions": "Detroit,MI",
+        "Colts": "Indianapolis,IN",
+        "Texans": "Houston,TX",
+        "Titans": "Nashville,TN",
+        "Jaguars": "Jacksonville,FL",
+        "Bills": "Orchard_Park,NY",
+        "Commanders": "Landover,MD",
+        "Redskins": "Landover,MD",
+        "Chargers": "Inglewood,CA",
+    }
 
-        sample_data = pd.DataFrame(
-            [[sample_year, sample_week, sample_winner, sample_day, sample_date, sample_time, sample_pts_win, sample_pts_loss, sample_home_team, sample_away_team, sample_home_team_previous_year_performance, sample_away_team_previous_year_performance, sample_home_team_current_year_performance, sample_away_team_current_year_performance, sample_home_team_current_sos, sample_away_team_current_sos, sample_stadium_name,sample_stadium_max_capacity,sample_stadium_regular_capacity,sample_home_city, sample_weather_condition]],
-            columns=["year","week","winner","day","date","time","pts_win","pts_loss","home_team_name","away_team_name","home_team_previous_year_performance","away_team_previous_year_performance","home_team_current_year_performance","away_team_current_year_performance","home_team_current_sos","away_team_current_sos","stadium_name","stadium_max_capacity","stadium_regular_capacity","home_city","weather_condition"])
+    sample_year = sample_date.year
+    sample_day = days_week[sample_date.weekday()]
+    sample_winner = home_team_names[sample_home_team]
+    sample_stadium_name = stadium_names[sample_home_team]
+    sample_stadium_max_capacity = stadium_max_capacities[sample_home_team]
+    sample_stadium_regular_capacity = stadium_regular_capacities[sample_home_team]
+    sample_home_city = cities[sample_home_team]
 
-        results = []
+    sample_data = pd.DataFrame(
+        [[sample_year, sample_week, sample_winner, sample_day, sample_date, sample_time, sample_pts_win, sample_pts_loss, sample_home_team, sample_away_team, sample_home_team_previous_year_performance, sample_away_team_previous_year_performance, sample_home_team_current_year_performance, sample_away_team_current_year_performance, sample_home_team_current_sos, sample_away_team_current_sos, sample_stadium_name,sample_stadium_max_capacity,sample_stadium_regular_capacity,sample_home_city, sample_weather_condition]],
+        columns=["year","week","winner","day","date","time","pts_win","pts_loss","home_team_name","away_team_name","home_team_previous_year_performance","away_team_previous_year_performance","home_team_current_year_performance","away_team_current_year_performance","home_team_current_sos","away_team_current_sos","stadium_name","stadium_max_capacity","stadium_regular_capacity","home_city","weather_condition"])
 
-        # Hace 100 iteraciones de la prediccion para calcular el promedio
-        for i in range(100):
-            # Identificar columnas numéricas y categóricas
-            numerical_features = sample_data.select_dtypes(include=["int64", "float64"]).columns
-            categorical_features = sample_data.select_dtypes(include=["object"]).columns
+    results = []
 
-            # Volver a calcular sets de entrenamiento y testeo
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    # Hace 100 iteraciones de la prediccion para calcular el promedio
+    for i in range(100):
+        # Identificar columnas numéricas y categóricas
+        numerical_features = sample_data.select_dtypes(include=["int64", "float64"]).columns
+        categorical_features = sample_data.select_dtypes(include=["object"]).columns
 
-            # Entrenar en el set de entrenamiento
-            lasso_model.fit(X_train, y_train)
+        # Volver a calcular sets de entrenamiento y testeo
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-            # Predecir en el set de testeo
-            lasso_sample_data_prediction = lasso_model.predict(sample_data)
+        # Entrenar en el set de entrenamiento
+        lasso_model.fit(X_train, y_train)
 
-            results.append(lasso_sample_data_prediction)
+        # Predecir en el set de testeo
+        lasso_sample_data_prediction = lasso_model.predict(sample_data)
 
-        st.session_state.results = np.array(results)
-        st.session_state.mean_result = np.mean(results)
-        st.toast("Se ha hecho la prediccion!", icon="🎉")
-    except Exception as e:
-        st.exception(e)
+        results.append(lasso_sample_data_prediction)
+
+    st.session_state.results = np.array(results)
+    st.session_state.mean_result = np.mean(results)
+    st.toast("Se ha hecho la prediccion!", icon="🎉")
 
 
 col1, col2, col3 = st.columns(3)
@@ -381,6 +379,7 @@ col1.metric("Precision del modelo lineal", str(np.round(linear_reg_score,4)*100)
 col2.metric("Precision del modelo ridge", str(np.round(ridge_score,4)*100)+"%")
 col3.metric("Precision del modelo lasso", str(np.round(lasso_score,4)*100)+"%")
 
+st.header("PRUBEA1")
 with st.form("Ingresa datos para realizar una prediccion"):
     sample_week = st.slider("Semana", 1, 17)
     sample_home_team = st.text_input("Equipo Local")
